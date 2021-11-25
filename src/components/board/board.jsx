62b7b2b0d12dcbce0d2c585ccd3c2b5ea31c6175
vpvm96 <vpvm96@gmail.com>
2021-11-25 20:02:25 +0900

    board_table, login form color

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -3,20 +3,43 @@ import styles from './board.module.css';
 
 const Board = () => {
   return (
-    // <div className={styles.container}>
-      <table>
-        <thead>
-          <tr>
-            <th>글번호</th>
-            <th>제목</th> 
-            <th>등록일</th>
-            <th>조회수</th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
-    // </div>
+    <div className={styles.container}>
+      <div style={{padding: "0 12px"}}>
+        <table className={styles.board_list}>
+          <colgroup>
+              <col width="5%" />
+              <col width="*" /> 
+              <col width="50%" />
+              <col width="*" />
+              <col width="*" />
+              <col width="*" />
+              <col width="*" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>구분</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일시</th>
+              <th>조회수</th>
+              <th>첨부</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>a</td>
+              <td>s</td>
+              <td>d</td>
+              <td>f</td>
+              <td>g</td>
+              <td>h</td>
+              <td>h</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/free.jsx
+++ src/components/navbar/free.jsx
@@ -3,13 +3,9 @@ import styles from './free.module.css'
 
 const Free = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.contents}>
-        <h4 className={styles.title}>
-          bbb
-        </h4>
-      </div>
-    </footer>
+    <div className={styles.container}>
+  
+    </div>
   )
 }
 
