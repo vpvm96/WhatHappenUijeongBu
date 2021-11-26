49541450ab77635611c1726af0d8c32eb9fa3b46
KimHanSeok <hanzang0706@naver.com>
2021-11-26 17:29:52 +0900

    sadd socket for sns

---
+++ src/network/socket.js
@@ -0,0 +1,23 @@
+import socket from 'socket.io-client';
+
+export default class Socket {
+    constructor(baseURL, getAccessToken) {
+        this.io = socket(baseURL, {
+            auth: (cb) => cb({ token: getAccessToken() }), // 쌉중요
+            auth: (cb) => cb({ token: localStorage.getItem('token') }),
+        });
+
+        this.io.on('connect_error', (err) => {
+            console.log('socket error', err.message);
+        });
+    }
+
+    onSync(event, callback) {
+        if(!this.io.connected) {
+            this.io.connect();
+        }
+
+        this.io.on(event, (message) => callback(message));
+        return () => this.io.off(event);
+    }
+}
---
+++ src/service/snsService.js
@@ -0,0 +1,50 @@
+export default class SnsService {
+    constructor(http, tokenStorage, socket) {
+      this.http = http;
+      this.tokenStorage = tokenStorage;
+      this.socket = socket;
+    }
+  
+    async getAllSns(username) {
+      const query = username ? `?username=${username}` : '';
+      return this.http.fetch(`/sns/${query}`, {
+        method: 'GET',
+        headers: this.getHeaders(),
+      });
+    }
+  
+    async postSns(text) {
+      return this.http.fetch(`/sns`, {
+        method: 'POST',
+        headers: this.getHeaders(),
+        body: JSON.stringify({ text, username: 'aaa', name: 'AAA' }),
+      });
+    }
+  
+    async deleteSns(snsId) { // no use
+      return this.http.fetch(`/sns/${snsId}`, {
+        method: 'DELETE',
+        headers: this.getHeaders(),
+      });
+    }
+  
+    async updateSms(snsId, text) { // no use
+      return this.http.fetch(`/sns/${snsId}`, {
+        method: 'PUT',
+        headers: this.getHeaders(),
+        body: JSON.stringify({ text }),
+      });
+    }
+  
+    getHeaders() { // for auth
+      // const token = this.tokenStorage.getToken();
+      const token = localStorage.getItem('token');
+      return {
+        Authorization: `Bearer ${token}`,
+      };
+    }
+  
+    onSync(callback) { // socket sync
+      return this.socket.onSync('sns', callback);
+    }
+  }
